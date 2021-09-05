501bdd9f2d70a2caf0925ccb906c5be892961a11
eia485 <eia485git@gmail.com>
2021-09-05 02:03:41 -0700

    remove unneeded namespaces

--- FixToString/FixToString.cs
+++ FixToString/FixToString.cs
@@ -1,12 +1,6 @@
 ﻿using HarmonyLib;
 using NeosModLoader;
-using System;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 using FrooxEngine;
-using FrooxEngine.LogiX;
 
 namespace FixToString
 {
